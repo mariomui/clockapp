fa975e927906cbcf528a871aada91fc9b242133a
mariomui <yi.kan.mui@gmail.com>
2019-03-31 10:07:33 -0700

    Installed netfliy and deployed

---
+++ client/src/components/DatePicker.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class DatePicker {
+  constructor(props) {
+    super(props)
+    this.state = {
+      alarmDayNum: '',
+      alarmDayWeek: '',
+      alarmMonth: '',
+      alarmYear: ''
+    }
+  }
+
+  render() {
+
+  }
+}
